d32b40edf4b8c4700d0be5dc0b1cd24764b1f77a
manbou310 <hare.sunshine09@gmail.com>
2021-06-18 18:33:42 +0900

    アソシエーションコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :items
-  has_many :records_lists
+  #has_many :items
+  #has_many :records_lists
   
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
     validates :birth_day
-    
+
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
       validates :last_name_zenkaku
       validates :first_name_zenkaku
