576a11e4fc17c60abcd85f4f26713cc234c3c2d9
manbou310 <hare.sunshine09@gmail.com>
2021-06-21 14:12:48 +0900

    ActiveHash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only:[:new]
+
+
   def index
   end
+
+  def new
+  end
+  
+  def create
+  end
+
+
+  private
+  params.require(:items).permit(:item_name, :image ,:description, :category_id, :status_id, :shopping_charge_id,
+                                :ship_form_id, :delivery_time_id, :price, :user).merge(user_id: current_user.id)
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,31 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   #has_one :record_list
+  has_one_attached :image
+
+ 
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shopping_charge
+  belongs_to :ship_form
+  belongs_to :delivery_time
+
+  validates :title, :text, presence: true
+  with_options numericality: { other_than: 1 } do
+    validates :category_id,
+    validates :status_id,
+    validates :shopping_charge_id,
+    validates :ship_form_id,
+    validates :delivery_time_id,
+  end
+
+  validates :price, presence: true,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 999999999 }
 
   with_options presence: true do
-    :item_name,
-    :description,
-    :price,
-    :category_id,
-    :status_id,
-    :shopping_charge_id,
-    :ship_form_id,
-    :delivery_time_id,
+    validates :item_name,
+    validates :description,
+    validates :image
   end
+end
